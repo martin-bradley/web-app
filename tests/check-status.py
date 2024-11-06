4d019115097666648b08e03714f5b22435670a5f
Martin Bradley <martinbradley87@icloud.com>
2024-11-06 16:39:11 +0000

    adding basic ping test and CI yml file

---
+++ tests/check-status.py
@@ -0,0 +1,15 @@
+import requests
+
+def ping_url(url):
+    try:
+        response = requests.get(url)
+        # Check if the response status code is 200 (OK)
+        if response.status_code == 200:
+            print(f'{url} is working.')
+        else:
+            print(f'{url} returned status code {response.status_code}.')
+    except requests.exceptions.RequestException as e:
+        print(f'Error: {e}')
+
+# Example usage
+ping_url('https://www.martinbradley.net')
